4f91aad63989004a121bc473be9e74d1df3d106b
zhigang.tan <tanzhig@163.com>
2016-04-24 23:16:55 +0800

    no message

---
+++ loach-web/src/main/java/org/tanziquan/loach/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package org.tanziquan.loach.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig extends
+        WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+    }
+}
